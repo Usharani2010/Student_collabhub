b7fb05ff4526090cc9ef8646b2d89aead983d327
Usha <usharani2010nani@gmail.com>
2025-07-12 14:03:24 +0530

    Revert "Jwt name field"
    This reverts commit c51ff7c6869c7caf6a692cf70a0447eb8b5960ab.

--- utils.py
+++ utils.py
@@ -7,7 +7,6 @@ KEY = "STUDENT_COLLABORATION_APP_KEY"
 def create_jwt_token(data: dict) -> str:
     payload = {
         "email": data.get("email"),
-        "name": data.get("name"),
         "exp": datetime.utcnow() + timedelta(hours=2)
     }
     token = jwt.encode(payload, KEY, algorithm="HS256")
